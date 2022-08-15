92010b9aae8ab2c9c4310884f86f589bfb753f33
rrosta <rrosta@yahoo.com>
2022-08-15 14:08:12 +0430

    manual migration / accounts created successfully

---
+++ KhoshHal/accounts/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    GENDER_CHOICES = (
+        ("M", "male"),
+        ("F", "female"),
+    )
+    USER_TYPE_CHOICES = (
+        (1, 'Patient'),
+        (2, 'Counselor'),
+        (3, 'Admin'),
+    )
+
+    email = models.EmailField(unique=True)
+    national_ID = models.PositiveIntegerField(blank=True, null=True)
+    phone = models.CharField(max_length=15)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, blank=True, null=True)
+    username = models.CharField(max_length=15, blank=True, null=True, unique=True)
+
+
+class Counselor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    specialty = models.CharField(max_length=128)
+    ME_number = models.PositiveIntegerField()
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.email
---
+++ KhoshHal/utils/__init__.py
@@ -0,0 +1,2 @@
+from .random_utils import RandomStringGen, random32, random64
+from .sms import sms_sender
