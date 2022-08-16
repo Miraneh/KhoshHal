7265a501428e9be612fd3566fddbffd154be917c
Hamila Mailee <hamilamailee77@gmail.com>
2022-08-17 01:09:52 +0430

    Created dummy profile page and connected login.html file to django
    Successfully tested methods "get" and "post" for login
    The problem was solved by commenting "django.contrib.auth.urls" in KhoshHal\urls.py

--- KhoshHal/accounts/urls.py
+++ KhoshHal/accounts/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LogInView.as_view(), name="login"),
-    path('login/<int:pk>/', LogInView.as_view()),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
+
--- KhoshHal/accounts/views.py
+++ KhoshHal/accounts/views.py
@@ -24,30 +24,30 @@ class SignUpView(APIView):
             serializer.is_valid(raise_exception=True)
         except:
             first_error = list(serializer.errors)[0]
-            return render(request, 'registration/signup.html', {'field':first_error,'error': serializer.errors[first_error][0]})
+            return render(request, 'registration/signup.html',
+                          {'field': first_error, 'error': serializer.errors[first_error][0]})
         serializer.create(validated_data=serializer.validated_data)
         # token, created = Token.objects.get_or_create(user=user)
         return render(request, 'registration/signup.html')
 
 
 class LogInView(APIView):  # TODO
-    print("In here")
-    template_name = "registration/login.html"
+
+    def get(self, request):
+        return render(request, "registration/login.html")
 
     def post(self, request, *args, **kwargs):
         print(request.data)
-        # serializer = LoginSerializers(data=request.data, context={'request': request})
+        return render(request, "registration/login.html")
+        # serializer = LoginSerializer(data=request.data, context={'request': request})
         # serializer.is_valid(raise_exception=True)
         # user = serializer.validated_data['user']
         # token, created = Token.objects.get_or_create(user=user)
         # return Response({"status": status.HTTP_200_OK, "Token": token.key})
 
-        email = request.POST.get('email')  # Get email value from form
-        password = request.POST.get('password')  # Get password value from form
-        user = authenticate(request, email=email, password=password)
 
-        if user is not None:
-            login(request, user)
-        # t   ype_obj = user_type.objects.get(user=user)
-        else:  # Invalid email or password. Handle as you wish
-            return redirect('home')
+class ProfileView(APIView):
+
+    def get(self, request):
+        print("hey hey")
+        return render(request, "registration/profile.html")
