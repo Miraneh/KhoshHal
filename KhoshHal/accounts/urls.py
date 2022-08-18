b844dafb8b5f4ba782a651e27c070f0f8e9b8dae
rrosta <rrosta@yahoo.com>
2022-08-19 02:58:45 +0430

    filter small change

--- KhoshHal/accounts/urls.py
+++ KhoshHal/accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", ProfileView.as_view(), name="profile"),
     # path("medical-info/", EditFileView.as_view(), name="upload medical information"),
-    path("search/", FilterView.as_view(filterset_class=UserFilter, template_name='index.html'), name="search")
+    # path("search/", FilterView.as_view(filterset_class=UserFilter, template_name='index.html'), name="search")
     # TODO make a section with search option
-    # path("search/", CounselorListView.as_view(), name="search"),
+    path("search/", CounselorListView.as_view(), name="search"),
 ]
--- KhoshHal/accounts/views.py
+++ KhoshHal/accounts/views.py
@@ -9,6 +9,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 class SignUpView(APIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
@@ -73,6 +74,7 @@ class ProfileView(APIView):
         print("hey hey")
         return render(request, "registration/profile.html")
 
+
 # class EditFileView(generics.UpdateAPIView):
 #     serializer_class = serializers.EditFileSerializer
 #     permission_classes = (IsCounselor,)
@@ -81,10 +83,10 @@ class ProfileView(APIView):
 #         return self.request.user
 #
 
-# class CounselorListView(generics.ListAPIView):
-#     serializer_class = UserSerializer
-#     queryset = Counselor.objects.all()
-#     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-#     search_fields = ['user__username', 'user__first_name', 'user__last_name', 'specialty']
-#     ordering_fields = ['rating', 'specialty']
-#     ordering = ['user__last_name']
+class CounselorListView(generics.ListAPIView):
+    serializer_class = CounselorSerializer
+    queryset = Counselor.objects.all()
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'specialty']
+    ordering_fields = ['rating', 'specialty']
+    ordering = ['user__last_name']
